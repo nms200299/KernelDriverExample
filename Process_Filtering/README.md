a5ab7525a476e93844d480910005050c2575f909
nms200299 <nms200299@naver.com>
2025-06-22 15:06:00 +0900

    프로세스 차단 구현

---
+++ Process_Filtering/README.md
@@ -0,0 +1,8 @@
+# PsSetCreateProcessNotifyRoutineEx를 이용한 프로세스 차단 구현
+
+* 작성자 : 2N(nms200299)
+
+* 설명 : 프로세스가 생성 시, 경로에 NOTEPAD.EXE가 포함되면 차단합니다.
+ 
+* 테스트 환경 : Hyper-V / Windows 10 22H2 x64 (19045.5965)
+
---
+++ Process_Filtering/src/FsFilter3.c
@@ -0,0 +1,109 @@
+﻿/*++
+
+Module Name:
+
+   .c
+
+Abstract:
+
+    PsSetCreateProcessNotifyRoutineEx을 이용한 프로세스 차단
+
+Environment:
+
+    Kernel mode
+
+--*/
+
+#include <fltKernel.h>
+#pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
+
+BOOLEAN ProcessNotifyAble = FALSE;
+
+/*************************************************************************
+    Prototypes
+*************************************************************************/
+
+EXTERN_C_START
+DRIVER_INITIALIZE DriverEntry;
+NTSTATUS
+DriverEntry (
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+    );
+VOID UnloadDriver(IN PDRIVER_OBJECT DriverObject);
+EXTERN_C_END
+
+
+/*************************************************************************
+    UserDefined ...
+*************************************************************************/
+
+void ProcessNotify(
+    _Inout_     PEPROCESS Process,
+    _In_        HANDLE ProcessId,
+    _Inout_opt_ PPS_CREATE_NOTIFY_INFO CreateInfo
+) {
+    UNREFERENCED_PARAMETER(Process);
+    if (KeGetCurrentIrql() > APC_LEVEL) {
+        return;
+    } // IRQL 체크
+
+    if (CreateInfo) {
+        UNICODE_STRING CmpUnicodeString;
+        RtlInitUnicodeString(&CmpUnicodeString, L"*\\NOTEPAD.EXE");
+
+        if (FsRtlIsNameInExpression(&CmpUnicodeString, (PUNICODE_STRING)CreateInfo->ImageFileName, TRUE, NULL)) {
+            CreateInfo->CreationStatus = STATUS_UNSUCCESSFUL;
+            DbgPrint("[DRIVER] Process Create Denied. [%wZ](%lu)\n", CreateInfo->ImageFileName, HandleToULong(ProcessId));
+            return;
+        }
+        DbgPrint("[DRIVER] Process Created. [%wZ](%lu)\n", CreateInfo->ImageFileName, HandleToULong(ProcessId));
+    } else {
+        DbgPrint("[DRIVER] Process Terminated. (%lu)\n", HandleToULong(ProcessId));
+    } // 프로세스 생성, 종료에 따른 디버그 메시지 출력
+
+    return;
+}
+
+/*************************************************************************
+    MiniFilter initialization and unload routines.
+*************************************************************************/
+
+NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+)
+{
+    NTSTATUS status;
+    UNREFERENCED_PARAMETER(RegistryPath);
+    DriverObject->DriverUnload = UnloadDriver;
+
+    status = PsSetCreateProcessNotifyRoutineEx(ProcessNotify, FALSE);
+    // PsSetCreateProcessNotifyRoutineEx() Callback 등록
+    if (NT_SUCCESS(status)) {
+        ProcessNotifyAble = TRUE;
+        DbgPrint("[DRIVER] PsSetCreateProcessNotifyRoutineEx Create Success\n");
+    } else {
+        DbgPrint("[DRIVER] PsSetCreateProcessNotifyRoutineEx Create Failed: 0x%08X\n", status);
+    }
+    return status;
+}
+
+VOID UnloadDriver(IN PDRIVER_OBJECT DriverObject)
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+
+    if (ProcessNotifyAble){
+        ProcessNotifyAble = FALSE;
+        NTSTATUS status;
+        status = PsSetCreateProcessNotifyRoutineEx(ProcessNotify, TRUE);
+        // PsSetCreateProcessNotifyRoutineEx() Callback 삭제
+        if (!NT_SUCCESS(status)) {
+            DbgPrint("[DRIVER] PsSetCreateProcessNotifyRoutineEx Remove Failed: 0x%08X\n", status);
+        } else {
+            DbgPrint("[DRIVER] PsSetCreateProcessNotifyRoutineEx Remove Success\n");
+        }
+
+    }
+}
