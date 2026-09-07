0e37b83bf66c2f4fa42a6d63edc52e266bd090e2
nms200299 <nms200299@naver.com>
2026-09-07 22:56:32 +0900

    [26.09.07] 커뮤니케이션 포트 예제 업로드

---
+++ FileSystem_Communication_Port/src/FsFilter3.c
@@ -0,0 +1,292 @@
+﻿/*++
+Abstract:
+    커뮤니케이션 포트를 통한 커널 드라이버 언로드 제어
+
+Environment:
+    Kernel mode
+
+--*/
+
+#include <fltKernel.h>
+#pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
+
+PFLT_FILTER gFilterHandle;
+
+/*************************************************************************
+    Prototypes
+*************************************************************************/
+
+EXTERN_C_START
+NTSTATUS DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+);
+NTSTATUS UnloadDriver(
+    _In_ FLT_FILTER_UNLOAD_FLAGS Flags
+);
+NTSTATUS FltConnectCallback(
+    PFLT_PORT ClientPort,
+    PVOID ServerPortCookie,
+    PVOID ConnectionContext,
+    ULONG SizeOfContext,
+    PVOID* ConnectionPortCookie
+);
+VOID FltDisconnectCallback(
+    PVOID ConnectionCookie
+);
+NTSTATUS FltMessageCallback(
+    PVOID PortCookie,
+    PVOID InputBuffer,
+    ULONG InputBufferLength,
+    PVOID OutputBuffer,
+    ULONG OutputBufferLength,
+    PULONG ReturnOutputBufferLength
+);
+EXTERN_C_END
+
+
+/*************************************************************************
+    UserDefined ...
+*************************************************************************/
+
+#define PORT_NAME			L"\\TEST_PORT"
+#define MAX_MESSAGE_LEN     1024
+
+typedef struct _CommStruct {
+    UINT8 Type;
+    WCHAR szMessage[MAX_MESSAGE_LEN];
+} CommStruct, * PCommStruct;
+
+PFLT_PORT g_pFltServerPort = NULL;
+PFLT_PORT g_pFltClientPort = NULL;
+BOOLEAN g_bUnloadFlag = TRUE;
+
+/*************************************************************************
+    MiniFilter initialization and unload routines.
+*************************************************************************/
+
+CONST FLT_REGISTRATION FilterRegistration = {
+    sizeof(FLT_REGISTRATION),         //  Size
+    FLT_REGISTRATION_VERSION,           //  Version
+    0,                                  //  Flags
+    NULL,                               //  Context
+    NULL,                               //  Operation callbacks
+    UnloadDriver,                               //  MiniFilterUnload 
+    NULL,                               //  InstanceSetup
+    NULL,                               //  InstanceQueryTeardown
+    NULL,                               //  InstanceTeardownStart
+    NULL,                               //  InstanceTeardownComplete
+    NULL,                               //  GenerateFileName
+    NULL,                               //  GenerateDestinationFileName
+    NULL                                //  NormalizeNameComponent
+};
+
+
+NTSTATUS DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+) {
+    NTSTATUS Status;
+    UNREFERENCED_PARAMETER(RegistryPath);
+    DbgPrint("[DRIVER] %s : Driver Load.\n", __func__);
+
+    Status = FltRegisterFilter(DriverObject,
+        &FilterRegistration,
+        &gFilterHandle);
+    if (!NT_SUCCESS(Status)) return Status;
+    // 필터 핸들 등록
+
+    PSECURITY_DESCRIPTOR pSd = NULL;
+    Status = FltBuildDefaultSecurityDescriptor(&pSd, FLT_PORT_ALL_ACCESS);
+    if (!NT_SUCCESS(Status)) goto FltUnReg;
+    // SecurityDescriptor 설정
+
+    UNICODE_STRING uniPortName;
+    RtlInitUnicodeString(&uniPortName, PORT_NAME);
+    OBJECT_ATTRIBUTES Oa = { 0 };
+    RtlZeroMemory(&Oa, sizeof(Oa));
+    InitializeObjectAttributes(&Oa, &uniPortName, OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE, NULL, pSd);
+    // Object Attribute 설정
+
+    Status = FltCreateCommunicationPort(gFilterHandle,
+        &g_pFltServerPort,
+        &Oa,
+        NULL,
+        FltConnectCallback,
+        FltDisconnectCallback,
+        FltMessageCallback,
+        1); // MaxConnections
+    // 커뮤니케이션 포트 등록
+
+    FltFreeSecurityDescriptor(pSd);
+    // pSd 할당 해제
+
+    if (!NT_SUCCESS(Status)) {
+    FltUnReg:
+        FltUnregisterFilter(gFilterHandle);
+        gFilterHandle = NULL;
+        return Status;
+    }
+
+    return Status;
+}
+
+NTSTATUS UnloadDriver(
+    _In_ FLT_FILTER_UNLOAD_FLAGS Flags
+) {
+    UNREFERENCED_PARAMETER(Flags);
+
+    if ((g_pFltClientPort) && (gFilterHandle)) {
+        CommStruct SendInfo, RepInfo;
+        //CommRepStruct RepInfo;
+        RtlZeroMemory(&SendInfo, sizeof(CommStruct));
+        RtlZeroMemory(&RepInfo, sizeof(CommStruct));
+
+        SendInfo.Type = 2;
+        wcscpy_s(SendInfo.szMessage, ARRAYSIZE(SendInfo.szMessage), L"Driver Unload Request");
+
+        NTSTATUS Status = STATUS_SUCCESS;
+        ULONG ReplyLength = sizeof(CommStruct);
+        Status = FltSendMessage(gFilterHandle,
+            &g_pFltClientPort,
+            &SendInfo,
+            sizeof(CommStruct),
+            &RepInfo,
+            &ReplyLength,
+            NULL);
+        // 클라이언트에게 메시지 전송
+
+        DbgPrint("[DRIVER] %s : Msg Send = %ls (%llu)\n",
+            __func__,
+            RepInfo.szMessage,
+            (ULONG64)(ULONG_PTR)PsGetCurrentProcessId());
+
+        if (NT_SUCCESS(Status)) {
+            if (RepInfo.Type == 2) {
+                if (RepInfo.szMessage[0] == L'0') {
+                    g_bUnloadFlag = FALSE;
+                } else {
+                    g_bUnloadFlag = TRUE;
+                } // 드라이버 언로드 플래그 설정
+
+                DbgPrint("[DRIVER] %s : Driver UnloadFlag Set %d\n",
+                    __func__,
+                    g_bUnloadFlag);
+            }
+        } else {
+            DbgPrint(
+                "FltSendMessage Status = 0x%08X, ReplyLength = %lu\n",
+                Status,
+                ReplyLength
+            );
+
+            g_bUnloadFlag = TRUE;
+            // 클라이언트 종료 등으로 FltSendMessage가 실패하면 드라이버 언로드 가능
+        }
+
+        if (!g_bUnloadFlag) {
+            DbgPrint("[DRIVER] %s : Driver Unload Denied.\n", __func__);
+            return STATUS_ACCESS_DENIED;
+        }
+        // g_bUnloadFlag가 False면 드라이버 언로드 방지
+
+        FltCloseClientPort(gFilterHandle, &g_pFltClientPort);
+        g_pFltClientPort = NULL;
+        // 클라이언트 포트 종료
+    }
+
+    if (g_pFltServerPort) {
+        FltCloseCommunicationPort(g_pFltServerPort);
+        g_pFltServerPort = NULL;
+    } // 서버 포트 종료
+
+    if (!gFilterHandle) return STATUS_SUCCESS;
+    FltUnregisterFilter(gFilterHandle);
+    gFilterHandle = NULL;
+    DbgPrint("[DRIVER] %s : Driver Unload.\n", __func__);
+    // 필터 등록 해제
+
+    return STATUS_SUCCESS;
+}
+
+/*************************************************************************
+    MiniFilter Communication routines.
+*************************************************************************/
+
+NTSTATUS FltConnectCallback(
+    PFLT_PORT ClientPort,
+    PVOID ServerPortCookie,
+    PVOID ConnectionContext,
+    ULONG SizeOfContext,
+    PVOID* ConnectionPortCookie
+) {
+    UNREFERENCED_PARAMETER(ServerPortCookie);
+    UNREFERENCED_PARAMETER(ConnectionContext);
+    UNREFERENCED_PARAMETER(SizeOfContext);
+    UNREFERENCED_PARAMETER(ConnectionPortCookie);
+
+    DbgPrint(
+        "[DRIVER] %s : Connected Client (%llu)\n",
+        __func__,
+        (ULONG64)(ULONG_PTR)PsGetCurrentProcessId());
+    if (!g_pFltClientPort) {
+        g_pFltClientPort = ClientPort;
+    }
+
+    return STATUS_SUCCESS;
+}
+
+VOID FltDisconnectCallback(
+    PVOID ConnectionCookie
+) {
+    UNREFERENCED_PARAMETER(ConnectionCookie);
+    DbgPrint(
+        "[DRIVER] %s : Disconnected Client (%llu)\n",
+        __func__,
+        (ULONG64)(ULONG_PTR)PsGetCurrentProcessId());
+    if (g_pFltClientPort) {
+        g_pFltClientPort = NULL;
+    }
+}
+
+
+NTSTATUS FltMessageCallback(
+    PVOID PortCookie,
+    PVOID InputBuffer,
+    ULONG InputBufferLength,
+    PVOID OutputBuffer,
+    ULONG OutputBufferLength,
+    PULONG ReturnOutputBufferLength
+) {
+    UNREFERENCED_PARAMETER(PortCookie);
+
+    if (InputBufferLength != sizeof(CommStruct)) return STATUS_SUCCESS;
+    if (!InputBuffer) return STATUS_SUCCESS;
+    CommStruct* ReciveInfo = (CommStruct*)InputBuffer;
+    // 입력 구조체 검증
+
+    switch (ReciveInfo->Type) {
+    case 1: {
+        // 클라이언트 요청에 대한 드라이버의 응답 처리  
+        DbgPrint("[DRIVER] %s : Msg Recive = %ls (%llu)\n",
+            __func__,
+            ReciveInfo->szMessage,
+            (ULONG64)(ULONG_PTR)PsGetCurrentProcessId());
+
+        if (OutputBufferLength != sizeof(CommStruct)) return STATUS_SUCCESS;
+        if (!OutputBuffer) return STATUS_SUCCESS;
+        CommStruct* SendInfo = (CommStruct*)OutputBuffer;
+        RtlZeroMemory(SendInfo, sizeof(CommStruct));
+        SendInfo->Type = 1;
+        wcscpy_s(SendInfo->szMessage, ARRAYSIZE(SendInfo->szMessage), L"Hello, User-Application");
+        DbgPrint("[DRIVER] %s : Msg Send = %ls (%llu)\n",
+            __func__,
+            SendInfo->szMessage,
+            (ULONG64)(ULONG_PTR)PsGetCurrentProcessId());
+        *ReturnOutputBufferLength = sizeof(CommStruct);
+        return STATUS_SUCCESS;
+        break;
+    }
+    }
+    return STATUS_SUCCESS;
+}
---
+++ FileSystem_Communication_Port/src/User-App/User-App.cpp
@@ -0,0 +1,110 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fltUser.h>
+
+// ####################################################
+
+#define PORT_NAME			L"\\TEST_PORT"
+#define MAX_MESSAGE_LEN     1024
+
+typedef struct _CommStruct {
+    UINT8 Type;
+    WCHAR szMessage[MAX_MESSAGE_LEN];
+} CommStruct, * PCommStruct;
+
+typedef struct _CommGetStruct {
+	FILTER_MESSAGE_HEADER hdr;
+	CommStruct data;
+} CommGetStruct, *PCommGetStruct;
+
+typedef struct _CommRepStruct {
+	FILTER_REPLY_HEADER hdr;
+	CommStruct data;
+} CommRepStruct, * PCommRepStruct;
+
+
+// ####################################################
+
+int main() {
+	HRESULT hResult;
+	HANDLE hPortHandle;
+
+	hResult = FilterConnectCommunicationPort(
+		PORT_NAME,
+		0,
+		NULL,
+		0,
+		NULL,
+		&hPortHandle
+	);
+	if (hResult != S_OK) {
+		printf("FilterConnectCommunicationPort() Fail(%x)\n", hResult);
+		return 1;
+	} // 커뮤니케이션 포트 연결
+
+
+	printf("[1. User -> Driver 통신]\n");
+
+	CommStruct SendInfo, ReciveInfo;
+	DWORD ReturnBytes = 0;
+
+	SendInfo.Type = 1;
+	wcscpy_s(SendInfo.szMessage, MAX_MESSAGE_LEN, L"Hello, Driver");
+	hResult = FilterSendMessage(
+		hPortHandle,
+		&SendInfo,
+		sizeof(CommStruct),
+		&ReciveInfo,
+		sizeof(CommStruct),
+		&ReturnBytes
+	);
+	if (hResult != S_OK) {
+		printf("FilterSendMessage() Fail(%x)\n", hResult);
+		return 1;
+	}
+	printf("Msg Send : %ls\n", SendInfo.szMessage);
+	// 드라이버한테 메시지 전송
+
+	if (ReturnBytes == sizeof(CommStruct)) {
+		printf("Msg Recive : %ls\n", ReciveInfo.szMessage);
+	} // 응답 크기가 일치하면 출력
+
+	printf("\n[2. Driver -> User 통신]\n");
+
+	CommGetStruct ReviceGetInfo;
+	ZeroMemory(&ReviceGetInfo, sizeof(ReviceGetInfo));
+	hResult = FilterGetMessage(hPortHandle, &ReviceGetInfo.hdr, sizeof(ReviceGetInfo), NULL);
+	if (hResult != S_OK) {
+		printf("FilterGetMessage() Fail(%x)\n", hResult);
+		return 1;
+	}
+	printf("Msg Get : %ls\n", ReviceGetInfo.data.szMessage);
+	// 드라이버한테 메시지 수신
+
+	BOOL bAllowFlag = 0;
+	printf("Allow driver unloading? (1/0) : ");
+	scanf_s("%d", &bAllowFlag);
+	printf("\n");
+
+	CommRepStruct ReplyInfo;
+	ZeroMemory(&ReplyInfo, sizeof(ReplyInfo));
+	ReplyInfo.hdr.MessageId = ReviceGetInfo.hdr.MessageId;
+	ReplyInfo.data.Type = 2;
+	if (bAllowFlag) {
+		wcscpy_s(ReplyInfo.data.szMessage, MAX_MESSAGE_LEN, L"1");
+	} else {
+		wcscpy_s(ReplyInfo.data.szMessage, MAX_MESSAGE_LEN, L"0");
+	}
+	hResult = FilterReplyMessage(hPortHandle, &ReplyInfo.hdr, sizeof(ReplyInfo.hdr) + sizeof(ReplyInfo.data));
+	if (hResult != S_OK) {
+		printf("FilterReplyMessage() Fail(%x)\n", hResult);
+		return 1;
+	}
+	printf("Msg Rep : %ls\n", ReplyInfo.data.szMessage);
+	// 수신한 메시지에 대한 회신
+
+	system("pause");
+	FilterClose(hPortHandle);
+    return 0;
+}
